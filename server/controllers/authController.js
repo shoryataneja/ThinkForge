2e78e65599d293c842bd1f45f079d6a77bcd621d
shoryataneja <shoryataneja5@gmail.com>
2026-02-10 05:29:04 +0530

    Implement login with JWT authentication

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,7 @@
 const User = require("../models/Temp");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
 
 const registerUser = async (req, res) => {
   try {
@@ -32,4 +34,44 @@ const registerUser = async (req, res) => {
   }
 };
 
-module.exports = { registerUser };
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ message: "All fields required" });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    // 🔑 create token
+    const token = jwt.sign(
+      { id: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    res.status(200).json({
+      message: "Login successful",
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+module.exports = { registerUser,loginUser };
