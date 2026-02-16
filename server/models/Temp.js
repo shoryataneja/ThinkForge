9e00993ad867d1da318de249b31a5945aea67a87
shoryataneja <shoryataneja5@gmail.com>
2026-02-16 19:56:19 +0530

    Updated user model

--- server/models/Temp.js
+++ server/models/Temp.js
@@ -4,7 +4,14 @@ const userSchema = new mongoose.Schema({
   name: String,
   email: { type: String, unique: true },
   password: String,
-  points: { type: Number, default: 0 }
+  points: { type: Number, default: 0 },
+  completedQuestions: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Question",
+    }
+  ]
 });
-
 module.exports = mongoose.model("User", userSchema);
+
+
